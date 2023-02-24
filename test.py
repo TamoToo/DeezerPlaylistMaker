52477f53d35fb0e93e72b39ce7ccee19bce30318
TamoToo <83399518+TamoToo@users.noreply.github.com>
2023-02-24 18:50:41 +0100

    Update test.py
    Remove keys to publish

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import deezer
 import requests
 import json
 
-access_token='frBaED5OSO90k05rvxvinPwOHYJmGT6m46kD6CFz6zlvt9sXzA'
-deezer_client = deezer.Client(app_id='534982', app_secret='e9a25ec2d6d09e95416770615222bed9', access_token=access_token)
+access_token=''
+deezer_client = deezer.Client(app_id='534982', app_secret='', access_token=access_token)
 playlist_id = 10190019482
 
 
