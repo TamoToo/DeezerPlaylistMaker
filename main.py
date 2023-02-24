8b405a490ab7b5e6fed665025166891b065a203c
TamoToo <83399518+TamoToo@users.noreply.github.com>
2023-02-24 18:50:00 +0100

    Update main.py
    Remove keys to publish

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 import deezer
 import asyncio
 
-access_token='frBaED5OSO90k05rvxvinPwOHYJmGT6m46kD6CFz6zlvt9sXzA'
-deezer_client = deezer.Client(app_id='534982', app_secret='e9a25ec2d6d09e95416770615222bed9', access_token=access_token)
+access_token=''
+deezer_client = deezer.Client(app_id='534982', app_secret='', access_token=access_token)
 playlist_id = 10190019482
 
 
@@ -130,4 +130,4 @@ async def playlist(ctx, cmd="", search_query=""):
     else:
         await ctx.send("try **!playlist help**")
 
-bot.run("OTU5NDM5NTkxMjE4MjE3MDEw.Ykb5wA.Mv2zKxoGzQoNgNx7W03PWAABDdk")
+bot.run("")
